d4d766227afa0c4d4ae4bf45d1647702520b85eb
swathihosagpudr <swathihosagoudr@gamil.com>
2026-01-16 19:52:36 +0530

    save

---
+++ test_code01.py
@@ -0,0 +1,7 @@
+
+from code01 import  celsius_to_fahrenheit
+
+# 1. Celsius to Fahrenheit
+def test_celsius_to_fahrenheit():
+    assert celsius_to_fahrenheit(0) == 32
+    assert celsius_to_fahrenheit(100) == 212
